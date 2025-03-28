cbdf58ed193c72b86eb65315d20cabb99859d34c
NavodContrath <navod.contrath@gmail.com>
2025-03-28 16:37:12 +0100

    implement data fetching for posts on component mount

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,31 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 
 function App() {
+  const [getPosts, setGetPosts] = useState([])
 
+
+  function fetchData() {
+    fetch('http://localhost:3000/posts')
+      .then(response => response.json())
+      .then(data => {
+        setGetPosts(data)
+      })
+      .catch(error => console.log({
+        error: "error",
+        message: error.message
+      }))
+  }
+
+  useEffect(() => {
+    fetchData()
+  }, [])
+
+  console.log(getPosts)
 
   return (
     <>
+
 
     </>
   )
